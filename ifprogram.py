6425a06cf71c367994d9581a79b2f5f26ecce8e7
anudita <39922334+anudita@users.noreply.github.com>
2018-06-17 19:20:44 +0530

    Add files via upload
    If, elif and else statements.
    This is a simple program to learn the use of if, elif and else keywords. This program finds whether the number entered by the user is negative or positive.

---
+++ ifprogram.py
@@ -0,0 +1,9 @@
+from time import sleep
+number = raw_input("Enter a number :")
+
+if number < 0:
+	print "Negative number"
+elif number == 0:
+	print "Number is zero"
+elif number > 0:
+	print "Positive number"
